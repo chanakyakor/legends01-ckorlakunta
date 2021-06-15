e63bd39919ab4cc76062126f4d6bca045a76ae55
chanakya korlakunta <ckorlakunta@etsy.com>
2021-06-15 12:27:07 -0400

    2

--- word-count-beam/src/main/java/org/apache/beam/examples/RecsDeliveredListing1.java
+++ word-count-beam/src/main/java/org/apache/beam/examples/RecsDeliveredListing1.java
@@ -4,8 +4,7 @@ import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.parquet.ParquetIO;
-import org.apache.beam.sdk.options.PipelineOptions;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.*;
 import org.apache.beam.sdk.transforms.*;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PValue;
@@ -17,6 +16,10 @@ import org.apache.parquet.hadoop.ParquetFileReader;
 import org.apache.parquet.hadoop.util.HadoopInputFile;
 import org.apache.parquet.schema.MessageType;
 import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.coders.AvroCoder;
+import org.apache.hadoop.fs.Path;
+
+
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -40,19 +43,40 @@ public static <T> T last(T[] array) {
 return array[array.length - 1];
 }
 
-public static void main(String[] args) throws IOException, ParseException {
+    public interface GetListingOptions extends PipelineOptions {
 
+        /**
+         * By default, this example reads from a public dataset containing the text of King Lear. Set
+         * this option to choose a different input file or glob.
+         */
+        @Description("Path of the file to read from")
+        @Default.String("gs://hadoop-sandbox-dev-default-sodf9k/fbahena/RecSysDeliveredListingsFlattenedWholeDay/2021_06_05/part-*.snappy.parquet")
+        String getInputFile();
 
-    PipelineOptions options = PipelineOptionsFactory.create();
+        void setInputFile(String value);
+
+        /** Set this required option to specify where to write the output. */
+        @Description("Path of the file to write to")
+        @Validation.Required
+        String getOutput();
+
+        void setOutput(String value);
+
+        @Description("CSV delimiter")
+        @Default.String(",")
+        String getCsvDelimiter();
+
+        void setCsvDelimiter(String value);
+    }
+
+    static void runJob(GetListingOptions options) throws IOException {
+
     Pipeline p = Pipeline.create(options);
 
-    String GCSPATH = "gs://hadoop-sandbox-dev-default-sodf9k/fbahena/RecSysDeliveredListingsFlattenedWholeDay/2021_06_05/part-*.snappy.parquet";
-//    String LOCALPATH = "/Users/ckorlakunta/development/word-count-dataflow/word-count-beam/src/main/java/org/apache/beam/examples/Flattened_2021-05-30_part-00000-cc2e0463-26fe-4ed1-bb09-368ce563f19f-c000.snappy.parquet";
-    String OUTPUTPATH = "gs://hadoop-sandbox-dev-default-sodf9k/ckorlakunta/recs_delivered_listings/dataflow_output1";
-    Schema AVSCHEMA = convert(GCSPATH);
-    System.out.println(AVSCHEMA);
+    String LOCALPATH = "/Users/ckorlakunta/development/word-count-dataflow/word-count-beam/src/main/java/org/apache/beam/examples/fbahena_RecSysDeliveredListingsFlattened_2021-05-30_part-00000-cc2e0463-26fe-4ed1-bb09-368ce563f19f-c000.snappy.parquet";
+    Schema schema = convert(new Path(LOCALPATH));
 
-    PCollection<GenericRecord> records = p.apply(ParquetIO.read(AVSCHEMA).from(GCSPATH));
+    PCollection<GenericRecord> records = p.apply("Read Features", ParquetIO.read(schema).from(options.getInputFile())).setCoder(AvroCoder.of(GenericRecord.class, schema));
     String delivered = "recommendations_module_delivered";
 
     PCollection<GenericRecord> recsDeliveredVisits = records.apply("Filter for delivery event type",
@@ -65,7 +89,7 @@ public static void main(String[] args) throws IOException, ParseException {
 
     PCollection<String> table2 = recsDeliveredVisits.apply(
             ParDo.of(new makeNewRow()));
-    table2.apply(TextIO.write().to(OUTPUTPATH));
+    table2.apply(TextIO.write().to(options.getOutput()));
 
     p.run().waitUntilFinish();
 }
@@ -105,15 +129,18 @@ public static void main(String[] args) throws IOException, ParseException {
 //    {"name":"event_source","type":["null","string"],"default":null},
 //    {"name":"is_bounce","type":"boolean"},
 
-    public static Schema convert (String parquetPath) throws IOException {
+    public static Schema convert (Path parquetPath) throws IOException {
 
         Configuration cfg = new Configuration();
         cfg.set("parquet.strict.typing", "true");
         cfg.set("parquet.avro.add-list-element-records", "true");
         cfg.set("parquet.avro.write-old-list-structure", "true");
 
+        //cfg.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem");
+        //cfg.set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS");
+
         // Create parquet reader
-        ParquetFileReader rdr = ParquetFileReader.open(HadoopInputFile.fromPath(new Path(parquetPath), cfg));
+        ParquetFileReader rdr = ParquetFileReader.open(HadoopInputFile.fromPath(parquetPath, cfg));
         // Get parquet schema
         MessageType schema = rdr.getFooter().getFileMetaData().getSchema();
         // Convert to Avro
@@ -121,4 +148,18 @@ public static void main(String[] args) throws IOException, ParseException {
         rdr.close();
         return avroSchema;
     }
+
+
+
+
+    public static void main(String[] args) throws IOException {
+        GetListingOptions options =
+                PipelineOptionsFactory.fromArgs(args).withValidation().as(GetListingOptions.class);
+
+        runJob(options);}
 }
+
+
+
+
+
